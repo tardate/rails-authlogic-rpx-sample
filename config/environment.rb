8e65a57d5d0f68264fc38b3d588ad0f21464127b
tardate <gallagher.paul@gmail.com>
2009-09-27 10:59:10 +0800

    update to authlogic_rpx 1.0.2

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "grosser-rpx_now", :lib => "rpx_now", :source => 'http://gems.github.com'
-  config.gem "tardate-authlogic_rpx", :lib => "authlogic_rpx", :source => 'http://gems.github.com'
+  config.gem "tardate-authlogic_rpx", :lib => "authlogic_rpx", :version => '>= 1.0.2', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
