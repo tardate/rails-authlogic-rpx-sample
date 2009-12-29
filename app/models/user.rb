42823b6b45d258dd069151ee23d77787b6b0445c
tardate <gallagher.paul@gmail.com>
2009-12-29 13:27:26 +0800

    update authlogic_rpx configuration for 1.1.0, and updte detail on user profile path

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
-	acts_as_authentic
-	account_merge_enabled true
+	acts_as_authentic do |c|
+		c.account_mapping_mode :auto
+		c.account_merge_enabled true
+	end
 	
 	attr_accessible :username, :email, :password, :password_confirmation, :rpx_identifier
 
 	validates_uniqueness_of   :username, :case_sensitive => false
-
+	
 private
 
 	# map_added_rpx_data maps additional fields from the RPX response into the user object during the "add RPX to existing account" process.
@@ -15,9 +17,27 @@ private
 	# "self" at this point is the user model. Map details as appropriate from the rpx_data structure provided.
 	#
 	def map_added_rpx_data( rpx_data )
-
 		# map some additional fields, e.g. photo_url
 		self.photo_url = rpx_data['profile']['photo'] if photo_url.blank?
 	end
-			
+
+	# before_merge_rpx_data provides a hook for application developers to perform data migration prior to the merging of user accounts.
+	# This method is called just before authlogic_rpx merges the user registration for 'from_user' into 'to_user'
+	# Authlogic_RPX is responsible for merging registration data.
+	#
+	# By default, it does not merge any other details (e.g. application data ownership)
+	#
+	def before_merge_rpx_data( from_user, to_user )
+		RAILS_DEFAULT_LOGGER.info "in before_merge_rpx_data"
+	end
+	
+	# after_merge_rpx_data provides a hook for application developers to perform account clean-up after authlogic_rpx has
+	# migrated registration details.
+	#
+	# By default, does nothing. It could, for example, be used to delete or disable the 'from_user' account
+	#
+	def after_merge_rpx_data( from_user, to_user )
+		RAILS_DEFAULT_LOGGER.info "in after_merge_rpx_data"	
+	end
+
 end
