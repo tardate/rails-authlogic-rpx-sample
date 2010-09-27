d5e490f7adf7f86c2eddc6e5f9a8701d9ca06c8b
Paul Gallagher <gallagher.paul@gmail.com>
2010-09-28 00:57:25 +0800

    filter_parameter_logging removed form app controller in rails 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  filter_parameter_logging :password, :password_confirmation
   
   helper_method :current_user, :current_user_session
   
