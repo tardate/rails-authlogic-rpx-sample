72ae4a1dd21f509db1b945e2a3e438f9cf67e22e
Paul Gallagher <gallagher.paul@gmail.com>
2010-09-28 00:55:09 +0800

    additional rails 3 framework files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAuthlogicRpxSample::Application.config.secret_token = '7c1b646c73c6cbe9fb7f21f7b4c66ddb832265d3b54afeda45e5356464e266f1d45c5dc059feacadec5fd4a575e35544f44cba53b5e9a5e055d8825315790521'
